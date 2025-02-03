0985afcb5f5188a5987ab02e113d5a9596f8aa73
mrwanawad <mrwaanawad@gmail.com>
2025-02-03 19:32:44 +0200

    final version of the project

--- user_window.py
+++ user_window.py
@@ -1,5 +1,28 @@
 from tkinter import *
 
+def check() : # normalization 
+    user_country = user_entry.get().capitalize() 
+    
+    with open('C:\\Users\\KimoStore\\Desktop\\Countries.txt') as file :
+        # file >>> represents the whole countries file
+        content = file.read()
+        
+        if user_country in content :
+            title_1['text'] = 'Book now !'
+            title_1['fg'] = 'green'
+            
+        else :
+            title_1['text'] = 'the country is not available !'
+            title_1['fg'] = 'red'                
+            
+            
+        
+        
+        
+        
+    
+    
+
 
 window = Tk()
 window.geometry('800x400')
@@ -13,7 +36,7 @@ user_entry = Entry(window, fg = 'black', bg = 'yellow',
 user_entry.place( x = 300, y = 100 )
 
 check_button = Button( window, text='click me to\n check the country', bg = 'yellow',
-                font = ('times new roman', 25) )
+                font = ('times new roman', 25), command= check )
 check_button.place(x = 200, y = 250)
 
 
